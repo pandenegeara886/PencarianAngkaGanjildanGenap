c82524d09fab555a20cf468228ce62a12319f793
pandenegeara886 <hawkingtzy@gmail.com>
2023-12-05 08:46:38 +0800

    init

--- Main.java
+++ Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
-        int b;
-        int case1;
+        int b, case1;
 
         System.out.print("a :");
         a = sc.nextInt();
